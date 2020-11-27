122b22f96a4bad88adf3b462809ede725ea95c33
Timothy Ayodele <timotech@yahoo.com>
2020-11-27 17:39:24 +0100

    Updates

---
+++ precache-manifest.83a9dc81e59c8d8e3ee6bb445a99660c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40276b86499f38603fe31ff8af5bd3f7",
+    "url": "/github-pages-wabpread/index.html"
+  },
+  {
+    "revision": "d1189165c4018f454eb6",
+    "url": "/github-pages-wabpread/static/js/2.f9cf2b41.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/github-pages-wabpread/static/js/2.f9cf2b41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e88fae79d62dd4eeab5",
+    "url": "/github-pages-wabpread/static/js/main.e88c9e59.chunk.js"
+  },
+  {
+    "revision": "c90751b650bdad3e4f85",
+    "url": "/github-pages-wabpread/static/js/runtime-main.281059d8.js"
+  }
+]);
